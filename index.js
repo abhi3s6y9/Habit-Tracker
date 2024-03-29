d046a71924dd49715d7c3e2d309c1b7d6a1e7ee9
Abhi <abhi3s6y9@gmail.com>
2024-03-29 08:55:38 +0530

    Styling the Home page using CSS

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const expressLayout = require('express-ejs-layouts');
 const bodyParser = require('body-parser');
 const port = 8000;
 
-// ------Statics files (css, js, sass) ------ //
+
+// ------Statics files (js, css, sass, etc) ------ //
 app.use(express.static(path.join(__dirname,'./assets')));
 // app.use(express.urlencoded()); (Old method)
 app.use(bodyParser.urlencoded({extended: false}));
@@ -17,12 +18,15 @@ app.set('views',path.join(__dirname, 'views'));
 
 // ------ EJS layouts ------//
 app.use(expressLayout);
+// extract style and scripts from sub pages into the layout
 app.set('layout extractStyles',true);
 app.set('layout extractScripts',true);
 
 
 // ------Router------------//
 app.use('/',require('./routes'));
+
+
 
 app.listen(port, function(err){
     if(err){
